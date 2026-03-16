6af8d41743d0bfa27104d1776ccc6f21a6b595d1
Timmy Darcy <timmydarcy44@github.com>
2026-03-16 10:31:16 +0100

    chore: drop deprecated next eslint config

--- next.config.js
+++ next.config.js
@@ -10,9 +10,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
